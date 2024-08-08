19650496e338ad5e0427cb77323b02b5a4b5512d
alie <mushyafaali@gmail.com>
2024-08-08 13:17:02 +0700

    try fetch cookie

--- src/app/(dashboard)/workspace/administrator/page.tsx
+++ src/app/(dashboard)/workspace/administrator/page.tsx
@@ -1,6 +1,12 @@
+"use client"
+
 import React from 'react'
+import Cookies from 'js-cookie'
+
 
 const homePage = () => {
+    const data = Cookies.get('data')
+    console.log('ini datanaya' + data);
     return (
         <div>homePage administrator</div>
     )
