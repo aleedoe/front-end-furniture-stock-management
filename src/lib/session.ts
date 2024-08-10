a1914b97136ea7931f5d1fab33d9df36c3f7b1a7
alie <mushyafaali@gmail.com>
2024-08-10 15:00:28 +0700

    repair param name

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,9 +1,9 @@
 import 'server-only'
 import { cookies } from 'next/headers'
 
-export function createSession(userId: string) {
+export function createSession(data: string) {
     const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
-    const sessionData = JSON.stringify({ userId, expiresAt })
+    const sessionData = JSON.stringify({ data, expiresAt })
 
     cookies().set('session', sessionData, {
         httpOnly: true,
