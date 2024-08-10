0a9b7c03af5a7377cd4e4f744a0c97ad28bce0d3
alie <mushyafaali@gmail.com>
2024-08-10 16:47:48 +0700

    add encription

--- src/app/(dashboard)/admin-login/actions/auth.ts
+++ src/app/(dashboard)/admin-login/actions/auth.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { axiosInstance } from "@/lib/axios";
+import { encrypt } from "@/lib/data-encript";
 import { cookies } from "next/headers";
 
 export async function handleLogin(
@@ -13,11 +14,14 @@ export async function handleLogin(
             username,
             password,
         });
-        
 
-        cookies().set("session", JSON.stringify(response.data));
-        // Assuming the token or user data is returned on successful login
+        // Enkripsi data sebelum menyimpannya dalam cookie
+        const encryptedData = encrypt(JSON.stringify(response.data));
+        cookies().set("session", encryptedData);
+
         console.log('Login successful:', response.data);
+        // const res = createSession(response.data);
+        // Redirect to another page, e.g., dashboard
         // router.push('/workspace/administrator');
     } catch (error) {
         console.error('Login failed:', error);
