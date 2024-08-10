a4a355c132f9a2bcd922cc41e31e8bbe9a055a46
alie <mushyafaali@gmail.com>
2024-08-10 15:02:54 +0700

    delete use cookie in internal user login

--- src/app/(dashboard)/admin-login/actions/auth.ts
+++ src/app/(dashboard)/admin-login/actions/auth.ts
@@ -1,16 +1,9 @@
 import { axiosInstance } from "@/lib/axios";
-import Cookies from 'js-cookie'
-
-// Use client-side cookies management, such as js-cookie
 
 export async function handleLogin(
-    event: React.FormEvent<HTMLFormElement>,
     username: string,
     password: string,
-    setError: React.Dispatch<React.SetStateAction<string>>,
-    router: any
 ) {
-    event.preventDefault();
 
     try {
         const response = await axiosInstance.post('/admin-login/', {
@@ -19,12 +12,10 @@ export async function handleLogin(
         });
 
         // Assuming the token or user data is returned on successful login
-        Cookies.set("data", JSON.stringify(response.data), { expires: 1 }); // Store in cookies for 1 day
         console.log('Login successful:', response.data);
         // Redirect to another page, e.g., dashboard
-        router.push('/workspace/administrator');
+        // router.push('/workspace/administrator');
     } catch (error) {
         console.error('Login failed:', error);
-        setError('Login failed. Please check your credentials and try again.');
     }
 }
