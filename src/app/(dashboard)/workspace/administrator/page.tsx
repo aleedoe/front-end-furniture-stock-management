668987c940c5b694a726baf2f669525d5148bf6f
alie <mushyafaali@gmail.com>
2024-08-17 22:09:06 +0700

    example using redux

--- src/app/(dashboard)/workspace/administrator/page.tsx
+++ src/app/(dashboard)/workspace/administrator/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client"
 
-const homePage = () => {
+import StoreProvider from '@/app/StoreProvider'
+import { Button } from '@/components/ui/button'
+import { decrement, increment, initializeCount } from '@/features/counter/counterSlice'
+import { RootState } from '@/lib/store'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const HomePage = () => {
+    const dispatch = useDispatch()
+    const count = useSelector((state: RootState) => state.counter.value)
+
+    // Initialize the count with a specific value
+    useEffect(() => {
+        dispatch(initializeCount(10)) // Initialize the counter with 10
+    }, [dispatch])
+
     return (
-        <div>homePage administrator</div>
+        <div>
+            <h1>Home Page Administrator</h1>
+            <p>Current Count: {count}</p>
+            <Button onClick={() => dispatch(increment())}>Increment</Button>
+            <Button onClick={() => dispatch(decrement())}>Decrement</Button>
+        </div>
     )
 }
 
-export default homePage
+const HomePageWithProvider = () => (
+    <StoreProvider count={0}>
+        <HomePage />
+    </StoreProvider>
+)
+
+export default HomePageWithProvider
