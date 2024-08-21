c73f5ccce4f0888035e20b941fd53d50371840f2
alie <mushyafaali@gmail.com>
2024-08-21 18:57:40 +0700

    disable middleware debuging

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const session = request.cookies.get("session");
-    console.log('mengambil cookie dari middleware: ', session?.value)
+    // console.log('mengambil cookie dari middleware: ', session?.value)
 
     
     
