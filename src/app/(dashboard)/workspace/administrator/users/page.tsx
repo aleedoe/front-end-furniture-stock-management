5474d957806336ea8a0b8a8a9ef78fe882cef632
alie <mushyafaali@gmail.com>
2024-08-22 12:48:04 +0700

    update css table

--- src/app/(dashboard)/workspace/administrator/users/page.tsx
+++ src/app/(dashboard)/workspace/administrator/users/page.tsx
@@ -1,11 +1,14 @@
+import { ScrollArea } from '@/components/ui/scroll-area'
 import MainTable from '@/components/workspace/MainTable'
 import React from 'react'
 
 const userPage = () => {
     return (
-        <div className=''>
-        <MainTable />
-        </div>
+        <>
+            <ScrollArea className="h-72">
+                <MainTable  />
+            </ScrollArea>
+        </>
     )
 }
 
--- src/components/workspace/MainTable.tsx
+++ src/components/workspace/MainTable.tsx
@@ -67,7 +67,7 @@ import {
 
 export default function MainTable() {
     return (
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <>
             <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
                 <Breadcrumb className="hidden md:flex">
                     <BreadcrumbList>
@@ -484,6 +484,6 @@ export default function MainTable() {
                     </TabsContent>
                 </Tabs>
             </main>
-        </div>
+        </>
     )
 }
