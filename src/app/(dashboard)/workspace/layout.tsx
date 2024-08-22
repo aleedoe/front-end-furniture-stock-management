5e331df006afd1ef258256dd4d21a8dd62ced7e8
alie <mushyafaali@gmail.com>
2024-08-22 12:24:31 +0700

    updated workspace layout

--- src/app/(dashboard)/workspace/layout.tsx
+++ src/app/(dashboard)/workspace/layout.tsx
@@ -44,7 +44,7 @@ export default function DashboardLayout({ children, }: { children: React.ReactNo
 
         fetchData();
     }, [userRole]);
-    
+
 
     const handleSetActiveItem = (label: string) => {
         setActiveItem(label);
@@ -106,18 +106,7 @@ export default function DashboardLayout({ children, }: { children: React.ReactNo
                         </DropdownMenu>
                     </header>
                     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-                        <div className="flex items-center">
-                            <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-                        </div>
-                        <div
-                            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-                        >
-                            <div className="flex flex-col items-center gap-1 text-center">
-                                <h3 className="text-2xl font-bold tracking-tight">
-                                    {children}
-                                </h3>
-                            </div>
-                        </div>
+                        {children}
                     </main>
                 </div>
             </div>
