8ef1cc2fb0b192f7c1b687741cb84ddb4f520e13
alie <mushyafaali@gmail.com>
2024-08-26 11:16:00 +0700

    add action in order page

--- src/app/(dashboard)/workspace/administrator/orders/page.tsx
+++ src/app/(dashboard)/workspace/administrator/orders/page.tsx
@@ -1,9 +1,59 @@
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import React from 'react'
+import { LuFile, LuListFilter, LuPlusCircle } from 'react-icons/lu'
 
 const orderPage = () => {
     return (
-        <div className='bg-slate-900 h-[1000px]'>
-            <h1>orderPage</h1>
+        <div className='bg-slate-400 h-[1000px]'>
+            <Tabs defaultValue="all">
+                <div className="flex items-center">
+                    <TabsList>
+                        <TabsTrigger value="all">All</TabsTrigger>
+                        <TabsTrigger value="active">Active</TabsTrigger>
+                        <TabsTrigger value="draft">Draft</TabsTrigger>
+                        <TabsTrigger value="archived" className="hidden sm:flex">
+                            Archived
+                        </TabsTrigger>
+                    </TabsList>
+                    <div className="ml-auto flex items-center gap-2">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="outline" size="sm" className="h-8 gap-1">
+                                    <LuListFilter size={20} />
+                                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                        Filter
+                                    </span>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuCheckboxItem checked>
+                                    Active
+                                </DropdownMenuCheckboxItem>
+                                <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
+                                <DropdownMenuCheckboxItem>
+                                    Archived
+                                </DropdownMenuCheckboxItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                        <Button size="sm" variant="outline" className="h-8 gap-1">
+                            <LuFile size={20} />
+                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                Export
+                            </span>
+                        </Button>
+                        <Button size="sm" className="h-8 gap-1">
+                            <LuPlusCircle size={20} />
+                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                Add Product
+                            </span>
+                        </Button>
+                    </div>
+                </div>
+            </Tabs>
         </div>
     )
 }
