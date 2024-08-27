d276ecf29e197585ebbfdb3d60c6718c635b38c7
alie <mushyafaali@gmail.com>
2024-08-27 17:09:34 +0700

    recovery action table

--- src/app/dashboard/administrator/users/page.tsx
+++ src/app/dashboard/administrator/users/page.tsx
@@ -84,6 +84,39 @@ const UserPage = () => {
                             <TabsTrigger value="reseller">Reseller</TabsTrigger>
                         </TabsList>
                         <div className="ml-auto flex items-center gap-2">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button variant="outline" size="sm" className="h-8 gap-1">
+                                        <LuListFilter size={20} />
+                                        <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                            Filter
+                                        </span>
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end">
+                                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuCheckboxItem checked>
+                                        Active
+                                    </DropdownMenuCheckboxItem>
+                                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
+                                    <DropdownMenuCheckboxItem>
+                                        Archived
+                                    </DropdownMenuCheckboxItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                            <Button size="sm" variant="outline" className="h-8 gap-1">
+                                <LuFile size={20} />
+                                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                    Export
+                                </span>
+                            </Button>
+                            <Button size="sm" className="h-8 gap-1">
+                                <LuPlusCircle size={20} />
+                                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                    Add User
+                                </span>
+                            </Button>
                             {/* Other dropdown and button controls */}
                         </div>
                     </div>
