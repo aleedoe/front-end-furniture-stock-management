72899f001226bfb0eb9d51ffd6785c28d27384e8
alie <mushyafaali@gmail.com>
2024-09-02 11:27:02 +0700

    add fetch api for reseller

--- src/api/dashboard/administrator/users/actions.ts
+++ src/api/dashboard/administrator/users/actions.ts
@@ -31,3 +31,34 @@ export async function getUsers(page: number = 1) {
         return error;
     }
 }
+
+export async function getResellers(page: number = 1) {
+    try {
+        // Ambil data sesi menggunakan fungsi getSessionData()
+        const sessionData = await getSessionData();
+
+        let token = '';
+        if (sessionData) {
+            // Ambil token akses dari data sesi
+            token = sessionData.tokens.access;
+        }
+
+        // Buat konfigurasi dengan Authorization header
+        const config = {
+            headers: {
+                Accept: 'application/json',
+                // Authorization: `Bearer ${token}`,
+            },
+        };
+
+        // Kirim permintaan dengan header Authorization dan pagination
+        const response = await axiosInstance.get(`/reseller/?page=${page}`, config);
+
+        return response;
+        
+    } catch (error: any) {
+        console.error('Fetching data failed:', error);
+        // Return the error message
+        return error;
+    }
+}
