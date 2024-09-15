9b4deef341e8e3b8faf53087adfd827554b92639
alie <mushyafaali@gmail.com>
2024-09-15 11:19:08 +0700

    setup redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Toaster } from "@/components/ui/toaster"
+import StoreProvider from "./StoreProvider";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <StoreProvider>
+          {children}
+        </StoreProvider>
         <Toaster />
       </body>
     </html>
---
+++ src/lib/features/authUser/authUserSlice.ts
@@ -0,0 +1,58 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+// Define the shape of the data
+interface AccessRights {
+    id: number
+    name: string
+}
+
+interface UserData {
+    id: number
+    name: string
+    phone: number
+    email: string
+    password: string
+    access_rights: AccessRights
+}
+
+interface Tokens {
+    refresh: string
+    access: string
+}
+
+interface UserState {
+    data: UserData[]
+    tokens: Tokens[]
+}
+
+const initialState: UserState = {
+    data: [],
+    tokens: [],
+}
+
+// Create a slice of the Redux store for user data
+const authUserSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        // Reducer to save user data
+        saveUserData(state, action: PayloadAction<UserData>) {
+            state.data.push(action.payload)
+        },
+        // Reducer to save tokens
+        saveTokens(state, action: PayloadAction<Tokens>) {
+            state.tokens.push(action.payload)
+        },
+        // Optional: Clear all user data and tokens
+        clearUserData(state) {
+            state.data = []
+            state.tokens = []
+        },
+    },
+})
+
+// Export the actions created by the slice
+export const { saveUserData, saveTokens, clearUserData } = authUserSlice.actions
+
+// Export the reducer to be used in the store
+export default authUserSlice.reducer
